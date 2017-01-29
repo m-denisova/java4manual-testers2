8741ffda6fcd5238709f40d108defa00d673ce57
Maria Denisova <mdenisova456@gmail.com>
2017-01-29 20:18:43 +0300

    remove flipcharts

--- src/test/java/etl/EtlTest.java
+++ src/test/java/etl/EtlTest.java
@@ -4,6 +4,7 @@ import etl.domain.EtlController;
 import etl.domain.EtlException;
 import etl.domain.Loader;
 import etl.extractor.FileCsvExtractor;
+import etl.loader.FileXmlLoader;
 
 public class EtlTest {
     public static void main(String[] args) throws TestFailException {
